50464f21052d4c9d52947cd5cd64a892e8532476
Vitalii Horbenko <vhorbenko@solvexus.com>
2022-10-25 22:20:07 +0300

    fix the server bug

--- services/getBlocks.js
+++ services/getBlocks.js
@@ -6,18 +6,26 @@ const { API_KEY, BASE_URL } = process.env;
 
 const getLastBlock = async () => {
   await sleep(200);
-  const { data } = await axios.get(
-    `${BASE_URL}?module=proxy&action=eth_blockNumber&apikey=${API_KEY}`
-  );
-  return hexToDec(data.result);
+  try {
+    const { data } = await axios.get(
+      `${BASE_URL}?module=proxy&action=eth_blockNumber&apikey=${API_KEY}`
+    );
+    return hexToDec(data.result);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getBlockByNumber = async (blockNumber) => {
   await sleep(200);
-  const { data } = await axios.get(
-    `${BASE_URL}?module=proxy&action=eth_getBlockByNumber&tag=${blockNumber}&boolean=true&apikey=${API_KEY}`
-  );
-  return data?.result;
+  try {
+    const { data } = await axios.get(
+      `${BASE_URL}?module=proxy&action=eth_getBlockByNumber&tag=${blockNumber}&boolean=true&apikey=${API_KEY}`
+    );
+    return data?.result;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = { getLastBlock, getBlockByNumber };
